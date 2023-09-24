426a5783dcf6c93a2a7ec971b64c8c1c15544846
Mohammed AlSammak <msk170a@gmail.com>
2023-09-24 19:22:53 +0300

    .

--- pkg/farm/antBFS.go
+++ pkg/farm/antBFS.go
@@ -3,11 +3,9 @@ package farm
 func (farm *Farm) AntBFS() {
 	queue := &Queue{}
 
-	//var discovered map[*Room]bool
-	//var visited map[interface{}]bool
-	//var not_visited map[*Room]bool
-
+	//discovered := make(map[*Room]bool)
 	visited := make(map[*Room]bool)
+	//not_visited := make(map[*Room]bool)
 
 	queue.Enqueue(farm.end_room)
 	room := queue.Dequeue()
