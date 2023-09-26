fc497967cfa5298b6cff9ffcd897081021a7ff56
M-SK <legendary512@gmail.com>
2023-09-27 01:25:39 +0300

    code cleaning

--- main/main.go
+++ main/main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	var farm ants.Farm
 
-	namla1(&farm)
+	namla2(&farm)
 	/* Print farm */
 	farm.PrintFarm()
 	farm.AntBFS()
--- pkg/farm/antsim.go
+++ pkg/farm/antsim.go
@@ -2,28 +2,30 @@ package farm
 
 import "fmt"
 
+/* Starts the simulator with one step per call */
 func (farm *Farm) AntSim_Step() {
 
 	ants := farm.ants
 
 	/* Loop throgh each ant */
 	for ant_idx := range ants {
-
-		alt_tun := farm.Find_Min_Path(ants[ant_idx])
-		if (alt_tun.is_empty || alt_tun.end) && (!ants[ant_idx].discovered_rooms[alt_tun] && ants[ant_idx].moving) {
-			ants[ant_idx].discovered_rooms[ants[ant_idx].room] = true
-			ants[ant_idx].discovered_rooms[alt_tun] = true
-			ants[ant_idx].room.is_empty = true // flag current room as empty
-			ants[ant_idx].room = alt_tun       // go to next room
-			alt_tun.is_empty = false           // flag next room as not empty
-			if ants[ant_idx].room.end {
-				ants[ant_idx].moving = false
+		ant := ants[ant_idx]
+		alt_tun := farm.Find_Min_Path(ant)
+		if (alt_tun.is_empty || alt_tun.end) && (!ant.discovered_rooms[alt_tun] && ant.moving) {
+			ant.discovered_rooms[ant.room] = true
+			ant.discovered_rooms[alt_tun] = true
+			ant.room.is_empty = true // flag current room as empty
+			ant.room = alt_tun       // go to next room
+			alt_tun.is_empty = false // flag next room as not empty
+			if ant.room.end {
+				ant.moving = false
 			}
 		}
 	}
 
 }
 
+/* Starts the simulator until all ants are at the end room */
 func (farm *Farm) AntSim() {
 
 	Step := 1
@@ -36,6 +38,7 @@ func (farm *Farm) AntSim() {
 
 }
 
+/* Custom iterations for Ant simulator */
 func (farm *Farm) AntSim_Iter(iter int) {
 	iteration := 0
 	Step := 1
@@ -49,6 +52,7 @@ func (farm *Farm) AntSim_Iter(iter int) {
 
 }
 
+/* This function will find the minimum path the ant can take */
 func (farm *Farm) Find_Min_Path(ant *Ant) *Room {
 	min := 9999
 	temp := ant.room.tunnels.head.room
